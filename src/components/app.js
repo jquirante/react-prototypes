8a7020238db99ec9be4b14e6fa0042e07b72878d
Justen Quirante <jcquirante@gmail.com>
2018-12-23 00:34:43 -0800

    Completed Axios Prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import MoviesContainer from './movie_container';
 
+const App = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Axios Demo</h1>
+            <MoviesContainer/>
+        </div>
+    )
+}
 
+export default App;
